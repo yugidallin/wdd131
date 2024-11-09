bd970bf1195021710de2606cefe86bca9bbc0d9e
yugidallin <cab24009@byui.edu>
2024-11-09 11:40:15 +0800

    optimization

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,6 @@
 const year = document.querySelector("#currentYear");
 const today = new Date();
-
-year.innerHTML = `&copy${today.getFullYear()}`;
-
 let lastModDate = new Date(document.lastModified);
 
+year.innerHTML = `&copy${today.getFullYear()}`;
 document.getElementById("lastModified").textContent = `Last modified on: ${lastModDate.toDateString()} at ${lastModDate.toLocaleTimeString()}`;
